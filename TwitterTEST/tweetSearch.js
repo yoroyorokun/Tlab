9d1a21cf0ee8dca7580e4de5275a2390cdaa6067
yoroyorokun <yoroyorokun@gmail.com>
2012-05-11 23:54:28 +0900

    Tweet Class created.

--- TwitterTEST/tweetSearch.js
+++ TwitterTEST/tweetSearch.js
@@ -11,13 +11,13 @@ function twitterSearchCallback(searchs) {
   var s = convertSearchedTweets(searchs);
   var element = document.createElement();
   element.innerHTML = s; 
-//  document.getElementById("twitter_update_list").appendChild(element);//innerHTML = "<ul>"+s+"</ul>";
   document.getElementById("twitter_update_list").insertBefore(element);//innerHTML = "<ul>"+s+"</ul>";
 }
 
 function convertSearchedTweets(searchs) {
   var s="";
   for(var i in searchs["results"]){
+	/**/
     var username = searchs["results"][i].from_user;
     var id = searchs["results"][i].id;
     var profile_image_url = searchs["results"][i].profile_image_url;
@@ -37,15 +37,22 @@ function convertSearchedTweets(searchs) {
 	s+=	'<span>' +status + " Post at " + form_date(time) + '</span>';
 	//s+=	'<a style="font-size:85%" href="http://twitter.com/' +username+ '/statuses/' +id+ '">' + reltime+ '</a>';
 	s+=	'</li>';
-	/* Imageオブジェクトを生成 */
+	/**/
+	var tweet = new Tweet(searchs["results"][i]);
+	if(tweet != null)
+		world.tweetList.push(tweet);
+	//console.log(searchs["results"][i]);
+	
+	/*
+	// Imageオブジェクトを生成 
 	imgs.push(new Image());
 	imgs[imgs.length-1].src = profile_image_url ;
-	/* 画像を描画 */
+	// 画像を描画 //
 	imgs[imgs.length-1].src = profile_image_url + "?" + new Date().getTime();
-	/* 画像が読み込まれるのを待ってから処理を続行 */
+	// 画像が読み込まれるのを待ってから処理を続行 //
 	imgs[imgs.length-1].onload = function() {
 		ctx.drawImage(this, Math.floor( Math.random() * 540 ),Math.floor( Math.random() * 380 ));
-	}
+	}*/
 	//ctx.drawImage(img, 0, 0,img.width,img.height,0,0);
   }
   return s;
@@ -58,18 +65,85 @@ function form_date(date) {
 	return ( date.getFullYear() + "/" + (date.getMonth()+1) +"/" + d + " " + date.getHours() + ":" +date.getMinutes()+ ":" +date.getSeconds() );
 }
 
-//Fri, 11 May 2012 06:11:52 +0000
+var World = function() {
 
-function callJSONP(url) {
-  var target = document.createElement('script');
-  target.charset = 'utf-8';
-  target.src = url;
-  document.body.appendChild(target);
+	this.step = function(){
+		//console.log("step enter");
+		for(var i in this.tweetList){
+			world.tweetList[i].step();
+		}
+	};
+	
+	this.draw = function(){
+		//console.log("draw enter");
+		this.ctx.clearRect(0,0,640,480);
+		
+		for(var i in this.tweetList){
+			world.tweetList[i].draw(world.ctx);
+		}
+	};
+	this.loop = function(){
+		//console.log("loop enter");
+		world.step();
+		world.draw();
+	};
+	
+	this.canvas = document.getElementById('canvas');
+	this.ctx = this.canvas.getContext('2d');
+	this.tweetList = new Array;
+	//var imgs = new Array;
+	updateLoop("#nicovideo", 30);
+	
 }
 
-var canvas = document.getElementById('canvas');
-if ( ! canvas || ! canvas.getContext ) { return false; }
-var ctx = canvas.getContext('2d');
-  var imgs = new Array;
+var Tweet = function(date) {
+	this.loadDone = function(t){
+		t.img_load_done = true;
+		//t.draw_state = 0;
+	}
 
-updateLoop("#nicovideo", 30);
+	if(world.tweetList[date.text] == null){
+		this.username = date.from_user;
+		this.id = date.id;
+		this.icon_url = date.profile_image_url;
+		this.status = date.text;
+		/*.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
+		  return '<a href="'+url+'">'+url+'</a>';
+		}).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
+		  return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+		});*/
+		var time_raw = date.created_at;
+		var time_arr = time_raw.match(/([a-zA-Z]+),\s([0-9]+)\s([a-zA-Z]+)\s([0-9]+)\s([0-9:]+)\s/);
+		var time_formed = time_arr[3] + " " + time_arr[2] + ", " + time_arr[4] + " " + time_arr[5];
+		this.time = new Date( time_formed );
+		//this.time = new Date(Date.UTC(this.time.getFullYear(),this.time.getMonth()+1,this.time.getDate(),this.time.getHours(),this.time.getMinutes(), this.time.getSeconds() ));
+		this.time = new Date( Date.UTC(this.time) );
+		
+		this.img = new Image();
+		this.img.src = this.icon_url + "?" + new Date().getTime();
+		this.img_load_done = false;
+		this.img.onload = this.loadDone(this);
+		this.draw_state = 0;
+	}else{ return null; };
+	
+	
+	
+	this.step = function(){};
+	this.draw = function(ctx){
+		if(this.img_load_done == true ){
+			ctx.drawImage(this.img, Math.floor( Math.random() * 540 ),Math.floor( Math.random() * 380 ));
+		}
+	};
+}
+
+
+function callJSONP(url) {
+	//console.log("callJSONP enter");
+	var target = document.createElement('script');
+	target.charset = 'utf-8';
+	target.src = url;
+	document.body.appendChild(target);
+}
+
+world = new World();
+setInterval( world.loop , 1000);
