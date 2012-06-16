e7940615c17709e2dc3a2535b48a5a9772e4cdf6
yoroyorokun <yoroyorokun@gmail.com>
2012-06-16 15:47:04 +0900

    tousihenkan

--- TwitterTEST/js/Animation.js
+++ TwitterTEST/js/Animation.js
@@ -85,13 +85,13 @@ var Animation = function(kind){
 			var ang = this.angle/(Math.PI/8);
 			if( this.kind != ANIME_CLASS.DANCE && this.kind != ANIME_CLASS.JUMP){
 				if( ang < 4 )
-					this.icon_trans = ang * 0.1 / 4;
+					this.icon_trans = ang * 0.1 ;
 				else if( ang < 8 )
-					this.icon_trans = ( ang - 8 ) * 0.1 /4;
+					this.icon_trans = ( ang - 8 ) * 0.1 ;
 				else if( ang < 12)
-					this.icon_trans = ( ang - 8 ) * 0.1 /4;
+					this.icon_trans = ( ang - 8 ) * 0.1 ;
 				else
-					this.icon_trans = (ang - 16 ) * 0.1 /4 ;
+					this.icon_trans = (ang - 16 ) * 0.1  ;
 			}else
 				this.icon_trans = 0;
 			
@@ -150,16 +150,9 @@ var Animation = function(kind){
 	
 		if( this.appearWaitTime >= this.jumppingTime )
 			return
-//		if( this.appearWaitTime < 200) //ジャンプして出てくるときになったら
-//		{
-	
-		// fall 処理は後回し
-		//if ( this.lifeTime > 50){
-	
-		//ctx.save();
-		//ctx.transform(WORLD_ZOOM_RATE,0,0,WORLD_ZOOM_RATE,0,0);
-		
-		// 86 : 200
+
+		var p = new Array;
+			
 		
 		//　画像を表示させる点（スクリーン座標）を原点とする世界へ移動
 		ctx.save();
@@ -169,116 +162,130 @@ var Animation = function(kind){
 		
 		ctx.translate(this.pos.x,this.pos.y);
 		
-		ctx.save();
-
-		ctx.transform( 1, this.icon_trans , 0 , 1- Math.abs(this.icon_trans)  ,this.icon_heni.x * WORLD_ZOOM_RATE , this.icon_heni.y * WORLD_ZOOM_RATE );
-//		ctx.translate( this.icon_heni.x * WORLD_ZOOM_RATE , this.icon_heni.y * WORLD_ZOOM_RATE );
-			
 		var iconLength = 86 * WORLD_ZOOM_RATE;
-		ctx.drawImage(tweet.img, -0.5 * iconLength, -0.5 * iconLength ,iconLength,iconLength);
+
+		p[0] = {x: this.icon_heni.x * WORLD_ZOOM_RATE -0.5 * iconLength , y: this.icon_heni.y * WORLD_ZOOM_RATE -0.5 * iconLength};
+		p[1] = {x: p[0].x + iconLength ,y:p[0].y + this.icon_trans * 20 };
+		p[2] = {x: p[0].x ,y:p[0].y + iconLength };
+		p[3] = {x: p[0].x + iconLength ,y:p[0].y + iconLength - this.icon_trans * 20 };
+		
+		
+		/* 左上三角のクリッピング */
+		ctx.save();
+		ctx.beginPath();ctx.moveTo( p[0].x , p[0].y);
+		ctx.lineTo( p[1].x , p[1].y);
+		ctx.lineTo( p[2].x , p[2].y);
+		ctx.closePath();
+		ctx.clip();
+		
+		var t1=(p[1].x-p[0].x) / iconLength;
+		var t2=(p[1].y-p[0].y) / iconLength;
+		var t3=(p[2].x-p[0].x) / iconLength;
+		var t4=(p[2].y-p[0].y) / iconLength;
+		var t5=p[0].x;
+		var t6=p[0].y;
+
+		ctx.transform( t1,t2,t3,t4,t5,t6);
+//		ctx.transform( 1, this.icon_trans , 0 , 1 ,this.icon_heni.x * WORLD_ZOOM_RATE -0.5 * iconLength , this.icon_heni.y * WORLD_ZOOM_RATE -0.5 * iconLength );
+		ctx.drawImage(tweet.img, 0, 0 ,iconLength,iconLength);
 
 		ctx.restore();
 		
-		ctx.transform( 1, this.icon_trans , 0 , 1- Math.abs(this.icon_trans)  ,0,0);
+		/* 右下三角のクリッピング */
+		ctx.save();
+		ctx.beginPath();
+		ctx.moveTo( p[1].x , p[1].y);
+		ctx.lineTo( p[2].x , p[2].y);
+		ctx.lineTo( p[3].x , p[3].y);
+		ctx.closePath();
+		ctx.clip();
 		
+		t1=(p[3].x-p[2].x) / iconLength;
+		t2=(p[3].y-p[2].y) / iconLength;
+		t3=(p[3].x-p[1].x) / iconLength;
+		t4=(p[3].y-p[1].y) / iconLength;
+		t5= p[2].x ;
+		t6= p[2].y;
+		
+		ctx.transform(t1,t2,t3,t4,t5,t6);
+		//ctx.transform( 1, -1* this.icon_trans , 0 , 1 -1* this.icon_trans ,this.icon_heni.x * WORLD_ZOOM_RATE -0.5 * iconLength , this.icon_heni.y * WORLD_ZOOM_RATE -0.5 * iconLength + iconLength);
+		ctx.drawImage(tweet.img, 0, -1 * iconLength ,iconLength,iconLength);
+
+		ctx.restore();
+
+		
+		
+		// キャラクタ表示部分
+		var nekoLength = 200 * WORLD_ZOOM_RATE;
+		p[0] = {x: -0.5 * nekoLength , y: -0.5 * nekoLength};
+		p[1] = {x: p[0].x + nekoLength ,y:p[0].y + this.icon_trans * 20 };
+		p[2] = {x: p[0].x ,y:p[0].y + nekoLength };
+		p[3] = {x: p[0].x + nekoLength ,y:p[0].y + nekoLength - this.icon_trans * 20 };
+		
+		/* 左上三角のクリッピング */
+		ctx.save();
+		ctx.beginPath();
+		ctx.moveTo( p[0].x , p[0].y);
+		ctx.lineTo( p[1].x , p[1].y);
+		ctx.lineTo( p[2].x , p[2].y);
+		ctx.closePath();
+		ctx.clip();
+		
+		t1=(p[1].x-p[0].x) / nekoLength;
+		t2=(p[1].y-p[0].y) / nekoLength;
+		t3=(p[2].x-p[0].x) / nekoLength;
+		t4=(p[2].y-p[0].y) / nekoLength;
+		t5=p[0].x;
+		t6=p[0].y;
+
 		if ( this.angle < Math.PI/2 || this.angle > Math.PI*3/2 )
 			// 反転
-			//ctx.transform(-1,0,0,1,tweet.screenPos.x+125, tweet.screenPos.y-125);
 			ctx.transform(-1,0,0,1,0,0);
-		else
-			//ctx.transform(1,0,0,1,tweet.screenPos.x-125, tweet.screenPos.y-125);
-			ctx.transform(1,0,0,1,0,0);
+			
+		ctx.transform( t1,t2,t3,t4,t5,t6);
 		
-	
-/*	
-		ctx.save();
-		ctx.translate(100,100);
-		ctx.drawImage(tweet.img, 0,0);
-//		ctx.drawImage(tweet.img, tweet.screenPos.x-25, tweet.screenPos.y-25,50,50);
-		ctx.restore();
-	*/
-		var nekoLength = 200 * WORLD_ZOOM_RATE;
 		if(this.appearWaitTime > 0)
 			ctx.translate(0,20 * WORLD_ZOOM_RATE);
 		if(this.image != null)
-			ctx.drawImage(this.image, -0.5*nekoLength , -0.5*nekoLength, nekoLength ,nekoLength);
-
+			//ctx.drawImage(this.image, -0.5*nekoLength , -0.5*nekoLength, nekoLength ,nekoLength);
+			ctx.drawImage(this.image, 0, 0, nekoLength ,nekoLength);
+		
 		ctx.restore();
 		
-/*		
-		if( this.kind < 30){
-			ctx.drawImage(ANIME_IMAGE,
-					Math.floor(this.sec / 10) * ANIME_WIDTH ,this.kind * ANIME_WIDTH,
-					ANIME_WIDTH,ANIME_WIDTH,
-					0, 0
-					,250,250);
-		}else{
-			ctx.drawImage(ANIME_IMAGE,
-					(this.kind % 30) * ANIME_WIDTH , 3 * ANIME_WIDTH,
-					ANIME_WIDTH,ANIME_WIDTH,
-					0, 0
-					,250,250);
-		}
-	*/	
-		//ctx.restore();
-		//ctx.drawImage(tweet.img, tweet.screenPos.x-25, tweet.screenPos.y-25,50,50);
-
-		// fall処理は後回し
-		/*
-		}
-		else if (this.lifeTime > 0)
-		{
-			ctx.save();
-			ctx.transform(WORLD_ZOOM_RATE,0,0,WORLD_ZOOM_RATE,0,0);
-				
-			ctx.beginPath();
-			ctx.moveTo(tweet.screenPos.x-125, tweet.screenPos.y-125);
-			ctx.lineTo(tweet.screenPos.x+125, tweet.screenPos.y-125);
-			ctx.lineTo(tweet.screenPos.x+125, tweet.screenPos.y+125);
-			ctx.lineTo(tweet.screenPos.x-125, tweet.screenPos.y+125);
-			ctx.clip();
-			
-			ctx.translate(0, (50 - this.lifeTime) * 250 / 50 / 2 );
-			
-			ctx.drawImage(tweet.img, tweet.screenPos.x-25, tweet.screenPos.y-25,50,50);
-			
-			if ( this.angle < Math.PI/2 || this.angle > Math.PI*3/2 )
-				ctx.transform(-1,0,0,1,tweet.screenPos.x+125, tweet.screenPos.y-125);
-			else
-				ctx.transform(1,0,0,1,tweet.screenPos.x-125, tweet.screenPos.y-125);
-
-			ctx.save();
-			ctx.translate(100,100);
-			ctx.drawImage(tweet.img, 0,0);
-			ctx.restore();
+		/* 右下三角のクリッピング */
+		ctx.save();
+		ctx.beginPath();
+		ctx.moveTo( p[1].x , p[1].y);
+		ctx.lineTo( p[2].x , p[2].y);
+		ctx.lineTo( p[3].x , p[3].y);
+		ctx.closePath();
+		ctx.clip();
 		
-			ctx.save();
-//			ctx.translate(0,this.lifeTime * 250 / 50);
-			ctx.transform(1,0,0,0.2,125, (this.lifeTime * 250 / 50 / 2) + 250/2 - 250*0.05 ); //250 );
-			ctx.beginPath();
-			ctx.arc(0,0,250/4,0,Math.PI*2 , false);
-			ctx.fillStyle="black";
-			ctx.fill();
-			ctx.restore();
+		t1=(p[3].x-p[2].x) / nekoLength;
+		t2=(p[3].y-p[2].y) / nekoLength;
+		t3=(p[3].x-p[1].x) / nekoLength;
+		t4=(p[3].y-p[1].y) / nekoLength;
+		t5= p[2].x ;
+		t6= p[2].y;
 		
-//			ctx.drawImage(ANIME_IMAGE,
-//				Math.floor(this.sec / 10) * ANIME_WIDTH ,this.kind * ANIME_WIDTH,
-//				ANIME_WIDTH,ANIME_WIDTH,
-//				0, 0
-//				,250,250);
+		if ( this.angle < Math.PI/2 || this.angle > Math.PI*3/2 )
+			// 反転
+			ctx.transform(-1,0,0,1,0,0);
+		
+		ctx.transform(t1,t2,t3,t4,t5,t6);
 			
-			ctx.drawImage(ANIME_IMAGE,
-				(this.kind % 30) * ANIME_WIDTH , 3 * ANIME_WIDTH,
-				ANIME_WIDTH,ANIME_WIDTH,
-				0, 0
-				,250,250);
+		
+		if(this.appearWaitTime > 0)
+			ctx.translate(0,20 * WORLD_ZOOM_RATE);
+		if(this.image != null)
+			//ctx.drawImage(this.image, -0.5*nekoLength , -0.5*nekoLength, nekoLength ,nekoLength);
+			ctx.drawImage(this.image, 0, -1 *nekoLength , nekoLength ,nekoLength);
+		
+		ctx.restore();
 
-				ctx.restore();
 			
-			
-		}*/
-
-//		};
+		ctx.restore();
+	
 	}
 
 }
