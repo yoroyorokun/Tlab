c2aca908c43dccccf30e88410e10c4ec4db8e908
yoroyorokun <yoroyorokun@gmail.com>
2012-09-25 15:32:17 +0900

    first commit

---
+++ keijiban/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keijiban::Application.config.secret_token = '3afbe8f1199292492e9de43e8fb5418e20117301bde61f88e7fbf3dd4a44476f375c75fb0c673cd90d0bbac5ccf3f748e43cd50a051c126134b6fd0cd89594dc'
